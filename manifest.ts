e82d2f642e12ca7870a2a1bf8fecef19bc71b6a4
Kazuhiro Sera <seratch@gmail.com>
2023-12-20 10:50:09 +0900

    Add Connect step examples

--- manifest.ts
+++ manifest.ts
@@ -2,15 +2,19 @@ import { Manifest } from "deno-slack-sdk/mod.ts";
 import SetupChannel from "./workflows/setup_channel.ts";
 import DailyStandup from "./workflows/daily_standup.ts";
 import MorningReminder from "./workflows/morning_reminder.ts";
+import GoogleCalendar from "./workflows/google_calendar.ts";
+import Giphy from "./workflows/giphy.ts";
 
 export default Manifest({
-  name: "Standards",
+  name: "My Standards",
   description: "A collection of useful standard workflows",
   icon: "assets/default_new_app_icon.png",
   workflows: [
     SetupChannel,
     DailyStandup,
     MorningReminder,
+    GoogleCalendar,
+    Giphy,
   ],
   botScopes: [
     "commands",
